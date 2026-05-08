265cfaf4e88b7793a855dfd3f90d129f108da663
Gaurav <117111964+Gaurav-Jang@users.noreply.github.com>
2026-05-08 21:59:38 +0530

    Update and rename api.js to api.jsx

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://team-task-manager-production-e350.up.railway.app/api",
+  baseURL: "https://team-task-manager-production-0cf8.up.railway.app/api",
 });
 
 export default api;
