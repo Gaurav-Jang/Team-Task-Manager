9e1e1573587933d0593d13cbc175ac1d4e57433d
Gaurav <117111964+Gaurav-Jang@users.noreply.github.com>
2026-05-08 21:05:47 +0530

    Update api.jsx

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://team-task-manager-production-e350.up.railway.app/api",
+  baseURL: "https://team-task-manager-production-e350.up.railway.app/api",
 });
 
 export default api;
